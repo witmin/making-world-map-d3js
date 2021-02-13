3f72c35402b019494c90ded5906d49a7a42b9b66
Millie Lin <linxinmin@gmail.com>
2021-02-13 16:47:28 +0800

    Created a world map

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import {} from 'd3';
+import { select, json, geoPath, geoNaturalEarth1 } from 'd3';
+import { feature } from 'topojson';
 
 const svg = select('svg');
 
-const width = +svg.attr('width');
-const height = +svg.attr('height'); // parseFloat() 可以用 + 快速实现
+const projection = geoNaturalEarth1();
+const pathGenerator = geoPath().projection(projection);
+
+svg.append('path')
+    .attr('class', 'sphere')
+    .attr('d', pathGenerator({type: 'Sphere'}));
+
+json('./countries-110m.json')
+    .then(data => {
+        const countries = feature(data, data.objects.countries);
+
+        const paths = svg.selectAll('path').data(countries.features)
+            .enter().append('path')
+            .attr('class', 'country')
+            .attr('d', d => pathGenerator(d))
+
+
+    });
