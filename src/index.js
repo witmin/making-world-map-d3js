b018719e91ebf02ae64fea095a45ce3d057252ad
Millie Lin <linxinmin@gmail.com>
2021-02-13 17:55:47 +0800

    Added hover color change, tooltip and zoom interaction(which doesn't work)

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
-import { select, json, geoPath, geoNaturalEarth1 } from 'd3';
-import { feature } from 'topojson';
+import {select, json, geoPath, geoNaturalEarth1, zoom, event} from 'd3';
+import {feature} from 'topojson';
 
 const svg = select('svg');
 
 const projection = geoNaturalEarth1();
 const pathGenerator = geoPath().projection(projection);
 
-svg.append('path')
+const g = svg.append('g');
+
+g.append('path')
     .attr('class', 'sphere')
     .attr('d', pathGenerator({type: 'Sphere'}));
+
+svg.call(zoom().on('zoom', ()=>{
+    g.attr('transform', event.transform);
+})); // Doesn't really work on Chrome.
 
 json('./countries-110m.json')
     .then(data => {
         const countries = feature(data, data.objects.countries);
 
-        const paths = svg.selectAll('path').data(countries.features)
+        const paths = g.selectAll('path').data(countries.features)
             .enter().append('path')
             .attr('class', 'country')
             .attr('d', d => pathGenerator(d))
-
-
+            .append('title')
+                .text(d => d.properties.name);
     });
